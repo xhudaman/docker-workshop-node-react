cedb993fc9deaa670613d00fcdad91d6427ac418
xhudaman <edell.matthew@gmail.com>
2021-10-16 02:51:04 -0400

    Fixed misleading db name

--- api/database/database.js
+++ api/database/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3");
 
-const database = new sqlite3.Database("./tasks.sqlite3", err => {
+const database = new sqlite3.Database("tasks.sqlite3", err => {
   if (err) {
     return console.error(err.message);
   }
